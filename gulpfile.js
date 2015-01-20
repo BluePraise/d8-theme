36eeeea4c1fd92cb711afe0b258720e15422974c
Magalie Chetrit <magaliechetrit@gmail.com>
2015-01-20 08:47:52 +0100

    jquery shizzle

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,52 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
+var compass = require('gulp-compass');
+var pngcrush = require('imagemin-pngcrush');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+// Compile Our Sass with Bundle[d] Compass
+gulp.task('sass', function() {
+  return gulp.src('./sass/*.scss')
+  .pipe(plugins.compass({
+    config_file: './config.rb',
+    css: 'stylesheets',
+    sass: 'sass',
+    bundle_exec: true
+  }))
+  .pipe(plugins.autoprefixer('last 2 version'))
+  .pipe(gulp.dest('stylesheets'));
 });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  // Watch js and scss file changes.
+  gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+
+// Make the images less of a fat bitch.
+gulp.task('images', function() {
+  return gulp.src('./images/*.{gif,jpg,png,svg}')
+    // Pass in options to the task
+    .pipe(plugins.imagemin({
+      progressive: true,
+      optimizationLevel: 5,
+      interlaced: true,
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest('./images/minified'));
+});
+
+// Default Task
+gulp.task('default', ['sass', 'watch']);
+
+// Maggie's own set of Gulp tasks
+gulp.task('speak', function() {
+  console.log("Hello, you are about to wrap it up.");
+});
+
+// Wraps up the project and make it ready for the
+// production environment. Minify CSS. Minify JS
+// Minify the images
+gulp.task('wrapitup', ['speak','images']);
+// gulp.watch(['images']);
